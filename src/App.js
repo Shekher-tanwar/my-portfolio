3d6fe3bd0a7c5d5a9e78fa39bafb937ee893e95a
shekhar-abacus <shekhar.tanwar@basiq360.in>
2026-04-22 12:50:15 +0530

    Changes in App.js

--- src/App.js
+++ src/App.js
@@ -260,7 +260,7 @@ const Portfolio = () => {
                   ))}
                 </div>
               </div>
-            ))}
+            ))} 
           </div>
         </div>
       </section>
